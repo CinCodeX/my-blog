49728e7d3b97267a5744771bbb2372780aae5ff1
CindyX <cindyxie@iinet.net.au>
2019-10-29 12:28:23 +1100

    First commit

---
+++ src/build/precache-manifest.84eb9c02821a19641a21cb6adde6f91f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "686c7e534845e2bd1f97ffe53db5a945",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75b42109de4f50a65a1f",
+    "url": "/static/css/main.73ff5b42.chunk.css"
+  },
+  {
+    "revision": "9424192d37120d072e21",
+    "url": "/static/js/2.98ab23cb.chunk.js"
+  },
+  {
+    "revision": "75b42109de4f50a65a1f",
+    "url": "/static/js/main.386e8333.chunk.js"
+  },
+  {
+    "revision": "afc0a052c644dce5e6bc",
+    "url": "/static/js/runtime-main.fbd335c9.js"
+  }
+]);
